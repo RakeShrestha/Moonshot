b7a20b933270c1c967c8dd844ba66d6331890ffc
RakeShrestha <orakeshstha7@gmail.com>
2025-01-05 20:56:53 +0545

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Rakesh Shrestha on 05/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
