e62d7dc33c47c08cc0e89173461f660613dfc884
RakeShrestha <orakeshstha7@gmail.com>
2025-01-06 21:21:30 +0545

    Added assets and made home screen

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Rakesh Shrestha on 06/01/2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
